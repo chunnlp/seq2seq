fe0fb6befeb3d108955153a6ed19b136022f77c3
ChunML <ahzhong11491@gmail.com>
2017-11-27 08:44:28 +0900

    Update seq2seq.py

--- seq2seq.py
+++ seq2seq.py
@@ -40,9 +40,25 @@ if __name__ == '__main__':
     X = np.array(X)
     y = np.array(y)
 
+    hidden_size = HIDDEN_DIM
+
     # Creating the network model
     print('[INFO] Compiling model...')
-    model = create_model(X_vocab_len, X_max_len, y_vocab_len, y_max_len, HIDDEN_DIM, LAYER_NUM)
+    encoder_inputs = Input(shape=(None, X_vocab_len))
+    encoder = LSTM(hidden_size, return_state=True)
+    encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+    encoder_states = [state_h, state_c]
+
+    decoder_inputs = Input(shape=(None, y_vocab_len))
+    decoder = LSTM(hidden_size, return_sequences=True, return_state=True)
+    decoder_outputs, _, _ = decoder(decoder_inputs,
+                                    initial_state=encoder_states)
+    decoder_dense = Dense(y_vocab_len, activation='softmax')
+    outputs = decoder_dense(decoder_outputs)
+    model = Model(inputs=[encoder_inputs, decoder_inputs], outputs=outputs)
+    model.compile(loss='categorical_crossentropy',
+            optimizer='rmsprop')
+    #model = create_model(X_vocab_len, X_max_len, y_vocab_len, y_max_len, HIDDEN_DIM, LAYER_NUM)
 
     # Finding trained weights of previous epoch if any
     saved_weights = find_checkpoint_file('.')
@@ -63,8 +79,8 @@ if __name__ == '__main__':
             # Shuffling the training data every epoch to avoid local minima
             indices = np.arange(len(X))
             np.random.shuffle(indices)
-            X = X[indices]
-            y = y[indices]
+            X = X[indices][:2000]
+            y = y[indices][:2000]
 
             X_sequences_input, y_sequences_input, y_sequences_target = process_data(X, y,
                                                                     X_max_len, y_max_len,
@@ -85,15 +101,46 @@ if __name__ == '__main__':
             print("The network hasn't been trained! Program will exit...")
             sys.exit()
         else:
-            X_test = load_test_data('test', X_word_to_ix, MAX_LEN)
-            X_test = pad_sequences(X_test, maxlen=X_max_len, dtype='int32')
             model.load_weights(saved_weights)
+        encoder_model = Model(encoder_inputs, encoder_states)
 
-            predictions = np.argmax(model.predict(X_test), axis=2)
-            sequences = []
-            for prediction in predictions:
-                sequence = ' '.join([y_ix_to_word[index] for index in prediction if index > 0])
-                print(sequence)
-                sequences.append(sequence)
-            np.savetxt('test_result', sequences, fmt='%s')
+        decoder_state_input_h = Input(shape=(hidden_size,))
+        decoder_state_input_c = Input(shape=(hidden_size,))
+        decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+        decoder_outputs, state_h, state_c = decoder(
+            decoder_inputs, initial_state=decoder_states_inputs)
+        decoder_states = [state_h, state_c]
+        decoder_outputs = decoder_dense(decoder_outputs)
+        decoder_model = Model(
+            [decoder_inputs] + decoder_states_inputs,
+            [decoder_outputs] + decoder_states)
+        X_sequences_input, _, _ = process_data(X, y,
+                                               X_max_len, y_max_len,
+                                               X_word_to_ix, y_word_to_ix)
+
+        max_target_sequence_length = 10
+        for ind in range(100):
+            input_sequence = X_sequences_input[ind:ind+1]
+            state_value = encoder_model.predict(input_sequence)
+            target_sequence = np.zeros((1, 1, y_vocab_len))
+            target_sequence[0, 0, 0] = 1.
+            stop_condition = False
+            target_sentence = ''
+            while not stop_condition:
+                outputs, h, c = decoder_model.predict(
+                    [target_sequence] + state_value)
+                sampled_token_index = np.argmax(outputs[0, -1, :])
+                output_word = y_ix_to_word[sampled_token_index]
+                target_sentence += output_word
+                if len(target_sentence) > max_target_sequence_length:
+                    stop_condition = True
+
+                target_sequence = np.zeros((1, 1, y_vocab_len))
+                target_sequence[0, 0, sampled_token_index] = 1.
+
+                states_values = [h, c]
+            print(target_sentence)
+            #predicted_sentence = decode_sentence(input_sequence)
+            #print(input_sentence)
+            #print(predicted_sentence)
 
