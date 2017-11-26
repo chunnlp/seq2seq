2fee2a40c9e4f12fcbb8dc76757729a13b58bbdf
ChunML <ahzhong11491@gmail.com>
2017-11-27 07:36:29 +0900

    Update seq2seq

--- seq2seq_utils.py
+++ seq2seq_utils.py
@@ -1,6 +1,6 @@
 from keras.preprocessing.text import text_to_word_sequence
-from keras.models import Sequential
-from keras.layers import Activation, TimeDistributed, Dense, RepeatVector, recurrent, Embedding
+from keras.models import Sequential, Model
+from keras.layers import Input, Activation, TimeDistributed, Dense, RepeatVector, recurrent, Embedding
 from keras.layers.recurrent import LSTM
 from keras.optimizers import Adam, RMSprop
 from nltk import FreqDist
@@ -21,6 +21,9 @@ def load_data(source, dist, max_len, vocab_size):
     # Splitting raw text into array of sequences
     X = [text_to_word_sequence(x)[::-1] for x, y in zip(X_data.split('\n'), y_data.split('\n')) if len(x) > 0 and len(y) > 0 and len(x) <= max_len and len(y) <= max_len]
     y = [text_to_word_sequence(y) for x, y in zip(X_data.split('\n'), y_data.split('\n')) if len(x) > 0 and len(y) > 0 and len(x) <= max_len and len(y) <= max_len]
+    for i, _ in enumerate(y):
+        y[i].insert(0, 'ZERO')
+    #print(y)
 
     # Creating the vocabulary set with the most common words
     dist = FreqDist(np.hstack(X))
@@ -47,7 +50,7 @@ def load_data(source, dist, max_len, vocab_size):
                 X[i][j] = X_word_to_ix['UNK']
 
     y_ix_to_word = [word[0] for word in y_vocab]
-    y_ix_to_word.insert(0, 'ZERO')
+    #y_ix_to_word.insert(0, 'ZERO')
     y_ix_to_word.append('UNK')
     y_word_to_ix = {word:ix for ix, word in enumerate(y_ix_to_word)}
     for i, sentence in enumerate(y):
@@ -56,7 +59,7 @@ def load_data(source, dist, max_len, vocab_size):
                 y[i][j] = y_word_to_ix[word]
             else:
                 y[i][j] = y_word_to_ix['UNK']
-    return (X, len(X_vocab)+2, X_word_to_ix, X_ix_to_word, y, len(y_vocab)+2, y_word_to_ix, y_ix_to_word)
+    return (X, len(X_vocab)+2, X_word_to_ix, X_ix_to_word, y, len(y_vocab)+1, y_word_to_ix, y_ix_to_word)
 
 def load_test_data(source, X_word_to_ix, max_len):
     f = open(source, 'r')
@@ -73,30 +76,59 @@ def load_test_data(source, X_word_to_ix, max_len):
     return X
 
 def create_model(X_vocab_len, X_max_len, y_vocab_len, y_max_len, hidden_size, num_layers):
-    model = Sequential()
 
-    # Creating encoder network
-    model.add(Embedding(X_vocab_len, 1000, input_length=X_max_len, mask_zero=True))
-    model.add(LSTM(hidden_size))
-    model.add(RepeatVector(y_max_len))
+    encoder_inputs = Input(shape=(None, X_vocab_len))
+    encoder = LSTM(hidden_size, return_state=True)
+    encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+    encoder_states = [state_h, state_c]
 
-    # Creating decoder network
-    for _ in range(num_layers):
-        model.add(LSTM(hidden_size, return_sequences=True))
-    model.add(TimeDistributed(Dense(y_vocab_len)))
-    model.add(Activation('softmax'))
+    decoder_inputs = Input(shape=(None, y_vocab_len))
+    decoder = LSTM(hidden_size, return_sequences=True, return_state=True)
+    decoder_outputs, _, _ = decoder(decoder_inputs,
+                                    initial_state=encoder_states)
+    decoder_dense = Dense(y_vocab_len, activation='softmax')
+    outputs = decoder_dense(decoder_outputs)
+    model = Model(inputs=[encoder_inputs, decoder_inputs], outputs=outputs)
     model.compile(loss='categorical_crossentropy',
-            optimizer='rmsprop',
-            metrics=['accuracy'])
+            optimizer='rmsprop')
     return model
 
-def process_data(word_sentences, max_len, word_to_ix):
+def create_infer_model(X_vocab_len, X_max_len, y_vocab_len, y_max_len, hidden_size, num_layers):
+    encoder_inputs = Input(shape=(None, X_vocab_len))
+    encoder = LSTM(hidden_size, return_state=True)
+    encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+    encoder_states = [state_h, state_c]
+
+    model = Model(encoder_inputs, encoder_states)
+
+    decoder_state_input_h = Input(shape=(hidden_size,))
+    decoder_state_input_c = Input(shape=(hidden_size,))
+    decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+    decoder_lstm = LSTM(hidden_size, return_sequences=True, return_state=True)
+    decoder_outputs, state_h, state_c = decoder_lstm(
+        decoder_inputs, initial_state=decoder_states_inputs)
+    decoder_states = [state_h, state_c]
+    decoder_dense = Dense(y_vocab_len, activation='softmax')
+    decoder_outputs = decoder_dense(decoder_outputs)
+    decoder_model = Model(
+        [decoder_inputs] + decoder_states_inputs,
+        [decoder_outputs] + decoder_states)
+    return decoder_model
+
+def process_data(X_word_sentences, y_word_sentences,
+                 X_max_len, y_max_len, X_word_to_ix, y_word_to_ix):
     # Vectorizing each element in each sequence
-    sequences = np.zeros((len(word_sentences), max_len, len(word_to_ix)))
-    for i, sentence in enumerate(word_sentences):
-        for j, word in enumerate(sentence):
-            sequences[i, j, word] = 1.
-    return sequences
+    encoder_input = np.zeros((len(X_word_sentences), X_max_len, len(X_word_to_ix)))
+    sequences_input = np.zeros((len(y_word_sentences), y_max_len, len(y_word_to_ix)))
+    sequences_target = np.zeros((len(y_word_sentences), y_max_len, len(y_word_to_ix)))
+    for i, (X_sentence, y_sentence) in enumerate(zip(X_word_sentences, y_word_sentences)):
+        for j, word in enumerate(X_sentence):
+            encoder_input[i, j, word] = 1.
+        for j, word in enumerate(y_sentence):
+            sequences_input[i, j, word] = 1.
+            if j > 0:
+                sequences_target[i, j - 1, word] = 1.
+    return encoder_input, sequences_input, sequences_target
 
 def find_checkpoint_file(folder):
     checkpoint_file = [f for f in os.listdir(folder) if 'checkpoint' in f]
